bc2edf9d9e40df00a1d774ddaa08847612adfd24
quyen.chau <quyen.chau@apps-cyclone.com>
2019-07-31 15:04:30 +0700

    +speech text google

--- app/src/main/java/com/example/mvvm_demo/activity/MainActivity.java
+++ app/src/main/java/com/example/mvvm_demo/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.example.mvvm_demo.R;
 import com.example.mvvm_demo.base.BaseActivity;
+import com.example.mvvm_demo.fragment.AudioFragment;
 
 public class MainActivity extends BaseActivity {
 
@@ -11,5 +12,6 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        replaceFragment(new AudioFragment(),false,R.id.actMain_content);
     }
 }
--- app/src/main/java/com/example/mvvm_demo/other/Constant.java
+++ app/src/main/java/com/example/mvvm_demo/other/Constant.java
@@ -1,5 +1,5 @@
 package com.example.mvvm_demo.other;
 
 public class Constant {
-
+    public final static int REQ_CODE_SPEECH_OUTPUT = 111;
 }
